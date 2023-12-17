508d89c9ad35cbc7db56109ee0855f33c4294601
Jason Jewell <jjkjj80@gmail.com>
2023-12-17 12:11:49 -0500

    create new todo function to execute on submit

---
+++ dist/appec6779adefbab82a97a4.bundle.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("#do"),t=(document.querySelector(".container"),document.querySelector(".sub")),c=(document.querySelector(".form"),document.querySelector(".desc")),o=document.querySelector(".due"),i=document.querySelector(".imp");class r{constructor(e,t,c,o){this.title=e,this.des=t,this.due=c,this.imp=o}incimp(){this.imp=this.imp+1}decimp(){this.imp=this.imp-1}}t.addEventListener("click",(()=>{new r(e,c,o,i),console.log(r)}))})();
+//# sourceMappingURL=appec6779adefbab82a97a4.bundle.js.map
--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 //dom lookup
-const input = document.querySelector('#do');
+
 const cont = document.querySelector('.container');
 const sub = document.querySelector('.sub')
-const form = document.querySelector('.form')
+const form = document.querySelector('.form');
+
 
 //dom manip
-class newtodo {
+class todo {
     constructor(title, description, due, importance) {
         this.title = title,
         this.des = description,
         this.due = due,
         this.imp = importance
     }
+
+    incimp () {
+        this.imp = this.imp + 1;
+    }
+
+    decimp () {
+        this.imp = this.imp -1;
+    }
 }
-sub.addEventListener('click', ())
+
+sub.addEventListener('click', (e) => {
+    e.preventDefault();
+    create()
+});
+
+function create() {
+    const title = document.querySelector('#do').value;
+    const desc = document.querySelector('.desc').value;
+    const due = document.querySelector('.due').value;
+    const imp = document.querySelector('.imp').value;
+    let task = new todo(title, desc, due, imp)
+    console.log(task)
+}
+
+function card () {
+
+}
